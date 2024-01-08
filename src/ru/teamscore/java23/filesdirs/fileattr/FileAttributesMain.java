1f03f5c2feb654d7009529c6b7e8f382868f6883
А.Коробецкая <kornast@yandex.ru>
2024-01-09 02:55:56 +0400

    Examples

--- src/ru/teamscore/java23/filesdirs/fileattr/FileAttributesMain.java
+++ src/ru/teamscore/java23/filesdirs/fileattr/FileAttributesMain.java
@@ -1,2 +1,58 @@
-package ru.teamscore.java23.filesdirs.fileattr;public class FileAttributesMain {
+package ru.teamscore.java23.filesdirs.fileattr;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.DosFileAttributes;
+import java.nio.file.attribute.PosixFileAttributes;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.Scanner;
+
+public class FileAttributesMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите путь к файлу: ");
+        Path file = Paths.get(sc.nextLine());
+
+        System.out.println("Absolute path: " + file.toAbsolutePath());
+        try {
+            BasicFileAttributes attr = Files.readAttributes(file, BasicFileAttributes.class);
+            System.out.println("creationTime: " + attr.creationTime());
+            System.out.println("lastAccessTime: " + attr.lastAccessTime());
+            System.out.println("lastModifiedTime: " + attr.lastModifiedTime());
+            System.out.println("isDirectory: " + attr.isDirectory());
+            System.out.println("isOther: " + attr.isOther());
+            System.out.println("isRegularFile: " + attr.isRegularFile());
+            System.out.println("isSymbolicLink: " + attr.isSymbolicLink());
+            System.out.println("size: " + attr.size() + " bytes");
+        } catch (IOException x) {
+            System.err.println("Cannot read basic file attributes: " + x.getMessage());
+        }
+
+        try {
+            DosFileAttributes attr = Files.readAttributes(file, DosFileAttributes.class);
+            System.out.println("DOS attributes");
+            System.out.println("isReadOnly is " + attr.isReadOnly());
+            System.out.println("isHidden is " + attr.isHidden());
+            System.out.println("isArchive is " + attr.isArchive());
+            System.out.println("isSystem is " + attr.isSystem());
+        } catch (UnsupportedOperationException x) {
+            System.err.println("DOS file attributes not supported:" + x);
+        } catch (IOException x) {
+            System.err.println("Some I/O error:" + x);
+        }
+
+        try {
+            PosixFileAttributes attr = Files.readAttributes(file, PosixFileAttributes.class);
+            System.out.println("POSIX attributes");
+            System.out.format("%s %s %s%n",
+                    attr.owner().getName(),
+                    attr.group().getName(),
+                    PosixFilePermissions.toString(attr.permissions()));
+        } catch (UnsupportedOperationException x) {
+            System.err.println("POSIX file attributes not supported:" + x);
+        } catch (IOException x) {
+            System.err.println("Some I/O error:" + x);
+        }
+    }
 }
--- src/ru/teamscore/java23/filesdirs/filesys/FileSysMain.java
+++ src/ru/teamscore/java23/filesdirs/filesys/FileSysMain.java
@@ -1,4 +1,30 @@
 package ru.teamscore.java23.filesdirs.filesys;
 
+import java.io.IOException;
+import java.nio.file.*;
+
 public class FileSysMain {
+    public static void main(String[] args) {
+        FileSystem fs = FileSystems.getDefault();
+
+        System.out.println("System separator: " + fs.getSeparator());
+
+        System.out.println("\nFile stores:");
+        System.out.format("%-20s %12s %12s %12s\n", "store", "total", "used", "avail");
+        for (FileStore store : fs.getFileStores()) {
+            try {
+                long total = store.getTotalSpace() / 1024;
+                long used = (store.getTotalSpace() - store.getUnallocatedSpace()) / 1024;
+                long avail = store.getUsableSpace() / 1024;
+                System.out.format("%-20s %12d %12d %12d\n", store, total, used, avail);
+            } catch (IOException e) {
+                System.err.println("Cannot read store '" + store.name() + "' space info: " + e.getMessage());
+            }
+        }
+
+        System.out.println("\nRoots:");
+        for (Path root : fs.getRootDirectories()) {
+            System.out.println(root);
+        }
+    }
 }
